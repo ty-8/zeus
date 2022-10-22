a40ee3c44ca1a5fe4144a59f271185b932e21375
Kevin Anantha (Kevan) <kevin.anantha@gmail.com>
2022-10-22 23:30:33 +0800

    chore: init tailwind

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(sum(21, 21));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
